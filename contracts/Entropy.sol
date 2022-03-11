bd293284831790daae2cbe602079283afafaa49c
roychong <roychongdev14@gmail.com>
2022-03-10 20:31:28 -0600

    update the test

--- contracts/Entropy.sol
+++ contracts/Entropy.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "hardhat/console.sol";
 
 error CardNotListed();
 error CardSaleHasEnded();
@@ -15,46 +16,51 @@ error InvalidDeck();
 error InvalidGeneration();
 error Unauthorized();
 
-struct CardListing { 
-    uint16 tokenId;    
+struct CardListing {
+    uint16 tokenId;
     uint32 startTime;
     address prevPurchaser;
 }
 
-contract Entropy is ERC721, Ownable, ReentrancyGuard {    
+contract Entropy is ERC721, Ownable, ReentrancyGuard {
     using Strings for uint256;
 
     uint8 public constant MAX_DECKS = 50;
-    uint8 public constant MAX_GENERATIONS = 60;    
+    uint8 public constant MAX_GENERATIONS = 60;
 
     uint24 public _listingDuration = 7200; // seconds
     uint16 public _chainPurchaseWindow = 3600; // seconds
     uint8 public _chainPurchaseDiscount = 25; // percent
-            
 
-    string public _baseTokenURI = "ipfs://foo";            
+    string public _baseTokenURI = "ipfs://foo";
     uint16 private _nextTokenId = 1;
     mapping(uint8 => mapping(uint8 => CardListing)) public _listings;
     uint8[] public _rarity;
-    
-    event CardListed(                
-        uint8 indexed deck, 
-        uint8 indexed generation, 
-        uint32 startTime        
-    );    
 
-    event CardPurchased(        
-        uint8 indexed deck, 
-        uint8 generation,   
-        uint16 indexed tokenId,         
-        address indexed purchaser            
-    );    
+    event CardListed(
+        uint8 indexed deck,
+        uint8 indexed generation,
+        uint32 startTime
+    );
 
-    constructor(uint8[] memory rarity) ERC721("Entropy", "ENRPY") {                     
-        _rarity = rarity;        
+    event CardPurchased(
+        uint8 indexed deck,
+        uint8 generation,
+        uint16 indexed tokenId,
+        address indexed purchaser
+    );
+
+    constructor(uint8[] memory rarity) ERC721("Entropy", "ENRPY") {
+        _rarity = rarity;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
@@ -66,45 +72,76 @@ contract Entropy is ERC721, Ownable, ReentrancyGuard {
         _baseTokenURI = baseTokenURI;
     }
 
-    function tokenURI(uint256 tokenId) public override view returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");        
-        return string(abi.encodePacked(_baseTokenURI,  "/", tokenId.toString(), ".json"));
-    }    
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    _baseTokenURI,
+                    "/",
+                    tokenId.toString(),
+                    ".json"
+                )
+            );
+    }
 
-    function _listCard(uint8 deckNum, uint8 genNum, uint32 startTime, address prevPurchaser) internal {
+    function _listCard(
+        uint8 deckNum,
+        uint8 genNum,
+        uint32 startTime,
+        address prevPurchaser
+    ) internal {
         if (deckNum > MAX_DECKS || deckNum == 0) revert InvalidDeck();
         if (genNum > MAX_GENERATIONS || genNum == 0) revert InvalidGeneration();
-        CardListing memory listing = _listings[deckNum][genNum];             
-        if (listing.tokenId == 0) { // Card has not been sold yet             
+        CardListing memory listing = _listings[deckNum][genNum];
+        if (listing.tokenId == 0) {
+            // Card has not been sold yet
             listing.startTime = startTime;
             listing.prevPurchaser = prevPurchaser;
             _listings[deckNum][genNum] = listing;
-            emit CardListed(deckNum, genNum, startTime);    
-        }        
+            emit CardListed(deckNum, genNum, startTime);
+        }
     }
 
-    /// @notice - Create auctions for all decks given a specific generation. Attempts to 
+    /// @notice - Create auctions for all decks given a specific generation. Attempts to
     //  list a card that has already been sold will be ignored.
-    function listGeneration(uint8 generation, uint32 startTime) external onlyOwner {
-        if (generation > MAX_GENERATIONS || generation == 0) revert InvalidGeneration();
-        for (uint8 i = 1; i <= MAX_DECKS; i++) {            
-            _listCard(i, generation, startTime, address(0));            
-        }        
+    function listGeneration(uint8 generation, uint32 startTime)
+        external
+        onlyOwner
+    {
+        if (generation > MAX_GENERATIONS || generation == 0)
+            revert InvalidGeneration();
+        for (uint8 i = 1; i <= MAX_DECKS; i++) {
+            _listCard(i, generation, startTime, address(0));
+        }
     }
 
     /// @notice - List a specific card for sale by deck number and generation number
-    function listCard(uint8 deckNum, uint8 genNum, uint32 startTime) external onlyOwner {        
-        if (_listings[deckNum][genNum].startTime != 0) revert CardSaleHasEnded();
-        for (uint8 i = 1; i <= MAX_DECKS; i++) {            
-            _listCard(i, genNum, startTime, address(0));            
-        }        
+    function listCard(
+        uint8 deckNum,
+        uint8 genNum,
+        uint32 startTime
+    ) external onlyOwner {
+        if (_listings[deckNum][genNum].startTime != 0)
+            revert CardSaleHasEnded();
+        for (uint8 i = 1; i <= MAX_DECKS; i++) {
+            _listCard(i, genNum, startTime, address(0));
+        }
     }
 
     /// @notice - List multiple cards by providing an array of deck numbers and generation numbers.
     //  Attempts to list cards that have already been sold will be ignored.
     function listManyCards(
-        uint8[] calldata deckNums, 
-        uint8[] calldata genNums, 
+        uint8[] calldata deckNums,
+        uint8[] calldata genNums,
         uint32 startTime
     ) external onlyOwner {
         for (uint8 i = 0; i < deckNums.length; i++) {
@@ -118,62 +155,83 @@ contract Entropy is ERC721, Ownable, ReentrancyGuard {
     /// deck (if any) will be able to purchase before startTime. Purchasing a card
     /// flags the current listing as ended by setting tokenId, mints the token to the purchaser, and lists
     /// the next card in the deck.
-    function purchaseCard(uint8 deckNum, uint8 genNum) external payable nonReentrant {        
-        if (deckNum > MAX_DECKS || deckNum == 0) revert InvalidDeck();   
+    function purchaseCard(uint8 deckNum, uint8 genNum)
+        external
+        payable
+        nonReentrant
+    {
+        if (deckNum > MAX_DECKS || deckNum == 0) revert InvalidDeck();
         if (genNum > MAX_GENERATIONS || genNum == 0) revert InvalidGeneration();
         CardListing memory listing = _listings[deckNum][genNum];
-        bool isChainPurchase = false;        
+        bool isChainPurchase = false;
         if (listing.startTime == 0) revert CardNotListed();
         if (listing.tokenId != 0) revert CardSaleHasEnded();
         if (block.timestamp < listing.startTime) {
-            if (listing.prevPurchaser == address(0) || msg.sender != listing.prevPurchaser) revert Unauthorized();
+            if (
+                listing.prevPurchaser == address(0) ||
+                msg.sender != listing.prevPurchaser
+            ) revert Unauthorized();
             isChainPurchase = true;
         }
-        uint price = isChainPurchase 
+        uint256 price = isChainPurchase
             ? _chainPrice(deckNum, genNum)
             : _price(deckNum, genNum, listing.startTime);
-        
-        if (msg.value < price) revert InsufficientFunds();    
-         
-        uint refund = msg.value - price;
+
+        if (msg.value < price) revert InsufficientFunds();
+
+        uint256 refund = msg.value - price;
         if (refund > 0) {
             (bool sent, ) = payable(msg.sender).call{value: refund}("");
             if (!sent) revert EthTransferFailed();
-        }             
+        }
 
         uint16 tokenId = _nextTokenId++;
         _listings[deckNum][genNum].tokenId = tokenId;
-        _safeMint(msg.sender, tokenId);       
-        
-        uint32 startTime = uint32(block.timestamp) + _chainPurchaseWindow;                                    
-        _listCard(deckNum, genNum+1, startTime, msg.sender);
-          
+        _safeMint(msg.sender, tokenId);
+
+        uint32 startTime = uint32(block.timestamp) + _chainPurchaseWindow;
+        _listCard(deckNum, genNum + 1, startTime, msg.sender);
+
         emit CardPurchased(deckNum, genNum, tokenId, msg.sender);
     }
 
     /// @notice - Rarity dependent price for normal purchases.
-    function _price(uint8 deckNum, uint8 genNum, uint32 startTime) public view returns (uint) {   
-        uint rarity = getRarity(deckNum, genNum);        
-        uint startPrice = ((rarity - 1) / 9) + 0.5 ether;             
+    function _price(
+        uint8 deckNum,
+        uint8 genNum,
+        uint32 startTime
+    ) public view returns (uint256) {
+        uint256 rarity = getRarity(deckNum, genNum);
+        uint256 startPrice = (((rarity - 1) * (1 ether)) / 9) + 0.5 ether;
         uint32 timeElapsed = uint32(block.timestamp) - startTime;
-        uint discountRate = startPrice / _listingDuration;
-        uint discount = discountRate * timeElapsed;
-        uint minPrice = startPrice / 10;
-        uint price = startPrice > discount ? startPrice - discount : minPrice;        
+        uint256 discountRate = startPrice / _listingDuration;
+        uint256 discount = discountRate * timeElapsed;
+        uint256 minPrice = startPrice / 10;
+        uint256 price = startPrice > discount
+            ? startPrice - discount
+            : minPrice;
         return price;
     }
 
     /// @notice - Rarity dependent price for chain purchases.
-    function _chainPrice(uint8 deckNum, uint8 genNum) private view returns (uint) {
-        uint rarity = getRarity(deckNum, genNum);        
-        uint startPrice = ((rarity - 1) / 9) + 0.5 ether;         
-        uint discount = (startPrice * _chainPurchaseDiscount) / 100;
+    function _chainPrice(uint8 deckNum, uint8 genNum)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rarity = getRarity(deckNum, genNum);
+        uint256 startPrice = (((rarity - 1) * (1 ether)) / 9) + 0.5 ether;
+        uint256 discount = (startPrice * _chainPurchaseDiscount) / 100;
         return startPrice - discount;
     }
 
     /// @dev - Helper to fetch rarity. Maps one dimensional array into two dimensional
     /// array keyed by deck number, generation number.
-    function getRarity(uint8 deckNum, uint8 genNum) internal view returns (uint8) {        
+    function getRarity(uint8 deckNum, uint8 genNum)
+        internal
+        view
+        returns (uint8)
+    {
         if (deckNum == 0 || deckNum > MAX_DECKS) revert InvalidDeck();
         if (genNum == 0 || genNum > MAX_GENERATIONS) revert InvalidDeck();
         uint8 deckIndex = (deckNum * 50) - 1;
@@ -182,10 +240,20 @@ contract Entropy is ERC721, Ownable, ReentrancyGuard {
     }
 
     /// @notice - Fetch tokenId for deck number, generation number pair (if exists).
-    function getTokenId(uint8 deckNum, uint8 genNum) external view returns (uint16) {
+    function getTokenId(uint8 deckNum, uint8 genNum)
+        external
+        view
+        returns (uint16)
+    {
         CardListing memory listing = _listings[deckNum][genNum];
-        require(listing.tokenId != 0, "ERC721Metadata: URI query for nonexistent token");
-        require(_exists(listing.tokenId), "ERC721Metadata: URI query for nonexistent token");
+        require(
+            listing.tokenId != 0,
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        require(
+            _exists(listing.tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
         return listing.tokenId;
     }
 
@@ -193,11 +261,17 @@ contract Entropy is ERC721, Ownable, ReentrancyGuard {
         _listingDuration = listingDuration;
     }
 
-    function setChainPurchaseWindow(uint16 chainPurchaseWindow) external onlyOwner {
+    function setChainPurchaseWindow(uint16 chainPurchaseWindow)
+        external
+        onlyOwner
+    {
         _chainPurchaseWindow = chainPurchaseWindow;
     }
 
-    function setChainPurchaseDiscount(uint8 chainPurchaseDiscount) external onlyOwner {
+    function setChainPurchaseDiscount(uint8 chainPurchaseDiscount)
+        external
+        onlyOwner
+    {
         _chainPurchaseDiscount = chainPurchaseDiscount;
-    }    
+    }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,26 @@
 import { expect } from "chai";
-import { ethers, } from "hardhat";
+import { ethers } from "hardhat";
 import { Entropy } from "../typechain";
 import "hardhat-gas-reporter";
 import { BigNumber, Contract, Signer } from "ethers";
 import { Provider } from "@ethersproject/abstract-provider";
 
 const getNow = () => Math.ceil(Date.now() / 1000);
-let owner: Signer, minter: Signer, buyer1: Signer, buyer2: Signer, entropy: Entropy;
+let owner: Signer,
+  minter: Signer,
+  buyer1: Signer,
+  buyer2: Signer,
+  entropy: Entropy;
 const startPrice = BigNumber.from(10).pow(18).mul(2); // 2 ETH
 const ONE_HOUR = 60 * 60;
 const ONE_DAY = 24 * 60 * 60;
 
 describe("Entropy Auctions", function () {
-
-
-  before( async () => {    
+  before(async () => {
     [owner, buyer1, buyer2] = await ethers.getSigners();
     const rarity: number[] = [];
     for (let i = 0; i < 3000; i++) {
-      rarity[i] = 5;
+      rarity[i] = 10;
     }
     const Entropy = await ethers.getContractFactory("Entropy");
     // @ts-ignore
@@ -26,217 +28,93 @@ describe("Entropy Auctions", function () {
     return entropy as Entropy;
   });
 
-
   it("Allows owner to start auctions for an entire generation", async () => {
-    const startTime = getNow() - ONE_HOUR;   
-    let cardSale = await entropy._listings(1,1);
+    const startTime = getNow() - ONE_HOUR;
+    let cardSale = await entropy._listings(1, 1);
     expect(cardSale.startTime).to.be.eq(0);
     await expect(entropy.listGeneration(1, startTime)).not.to.be.reverted;
-    cardSale = await entropy._listings(1,1);    
+    cardSale = await entropy._listings(1, 1);
     expect(cardSale.startTime).to.be.eq(startTime);
 
-    const cardSale2 = await entropy._listings(1,2);
-    console.log(cardSale2);
-    expect(cardSale2.startTime).to.be.eq(0);       
+    const cardSale2 = await entropy._listings(1, 2);
+    expect(cardSale2.startTime).to.be.eq(0);
   });
 
   it("Allows user to purchase card that is on sale.", async () => {
-      await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(0);    
-      await expect(entropy.ownerOf(1)).to.be.revertedWith("ERC721: owner query for nonexistent token");
-      await expect(entropy.connect(buyer1).purchaseCard(1, 1, {value: startPrice})).not.to.be.reverted;
-      await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(1);    
-      expect(await entropy.ownerOf(1)).to.be.eq(await buyer1.getAddress());
-      expect(await entropy.tokenURI(1)).to.be.eq('ipfs://foo/1.json');
-
-      const block = await ethers.provider.getBlockNumber();
-      const events = await entropy.queryFilter(
-        entropy.filters.CardPurchased(
-          null,
-          null,
-          null,
-          null,        
-        ),
-        block
-      );
-      expect(events.length).eq(1);
-      const eventDetails = entropy.interface.parseLog(events[0]);
-      expect(eventDetails.name).to.eq("CardPurchased");            
-      expect(eventDetails.args.deck).to.eq(1);
-      expect(eventDetails.args.generation).to.eq(1);
-      expect(eventDetails.args.tokenId).to.eq(1);
-      expect(eventDetails.args.purchaser).to.eq(await buyer1.getAddress());
-  });
-
-  it("Reverts when random user attempts chain purchase", async () => {    
-    await expect(entropy.connect(buyer2).purchaseCard(1, 2, {value: startPrice}))
-      .to.be.revertedWith('Unauthorized');    
-  });
-
-  it("Allows prev purchaser to make chain purchase", async () => {    
-    await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(1);    
-    await expect(entropy.ownerOf(2)).to.be.revertedWith("ERC721: owner query for nonexistent token");
-    await expect(entropy.connect(buyer1).purchaseCard(1, 2, {value: startPrice})).not.to.be.reverted;
-    await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(2);    
-    expect(await entropy.ownerOf(2)).to.be.eq(await buyer1.getAddress());
-    expect(await entropy.tokenURI(2)).to.be.eq('ipfs://foo/2.json');
-
-    const block = await ethers.provider.getBlockNumber();
-    const events = await entropy.queryFilter(
-      entropy.filters.CardPurchased(
-        null,
-        null,
-        null,
-        null,        
-      ),
-      block
+    await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(0);
+    await expect(entropy.ownerOf(1)).to.be.revertedWith(
+      "ERC721: owner query for nonexistent token"
     );
-    expect(events.length).eq(1);
-    const eventDetails = entropy.interface.parseLog(events[0]);
-    expect(eventDetails.name).to.eq("CardPurchased");            
-    expect(eventDetails.args.deck).to.eq(1);
-    expect(eventDetails.args.generation).to.eq(2);
-    expect(eventDetails.args.tokenId).to.eq(2);
-    expect(eventDetails.args.purchaser).to.eq(await buyer1.getAddress());
+    await expect(
+      entropy.connect(buyer1).purchaseCard(1, 2, { value: startPrice })
+    ).to.be.revertedWith("CardNotListed");
+    await expect(
+      entropy.connect(buyer1).purchaseCard(1, 1, { value: startPrice })
+    )
+      .to.emit(entropy, "CardPurchased")
+      .withArgs(1, 1, 1, await buyer1.getAddress());
+    await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(1);
+    expect(await entropy.ownerOf(1)).to.be.eq(await buyer1.getAddress());
+    expect(await entropy.tokenURI(1)).to.be.eq("ipfs://foo/1.json");
   });
 
-  it("Reverts when user tries to purchase card that has sold", async () => {    
-    await expect(entropy.connect(buyer1).purchaseCard(1, 1, {value: startPrice}))
-      .to.be.revertedWith('CardSaleHasEnded');    
+  it("Reverts when random user attempts chain purchase", async () => {
+    await expect(
+      entropy.connect(buyer2).purchaseCard(1, 2, { value: startPrice })
+    ).to.be.revertedWith("Unauthorized");
+  });
+
+  it("Allows prev purchaser to make chain purchase", async () => {
+    await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(1);
+    await expect(entropy.ownerOf(2)).to.be.revertedWith(
+      "ERC721: owner query for nonexistent token"
+    );
+    await expect(
+      entropy.connect(buyer1).purchaseCard(1, 2, { value: startPrice })
+    )
+      .to.emit(entropy, "CardPurchased")
+      .withArgs(1, 2, 2, await buyer1.getAddress());
+    await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(2);
+    expect(await entropy.ownerOf(2)).to.be.eq(await buyer1.getAddress());
+    expect(await entropy.tokenURI(2)).to.be.eq("ipfs://foo/2.json");
+  });
+
+  it("Reverts when user tries to purchase card that has sold", async () => {
+    await expect(
+      entropy.connect(buyer1).purchaseCard(1, 1, { value: startPrice })
+    ).to.be.revertedWith("CardSaleHasEnded");
   });
 
   it("Allows owner to start auctions for specific deck & generation.", async () => {
-    const startTime = getNow() - ONE_HOUR;   
-    let cardSale = await entropy._listings(3,5);
+    const startTime = getNow() - ONE_HOUR;
+    let cardSale = await entropy._listings(3, 5);
     expect(cardSale.startTime).to.be.eq(0);
     await expect(entropy.listCard(3, 5, startTime)).not.to.be.reverted;
-    cardSale = await entropy._listings(3,5);        
+    cardSale = await entropy._listings(3, 5);
     expect(cardSale.startTime).to.be.eq(startTime);
   });
 
   it("Allows user to purchase card that has been listed specifically", async () => {
-    await expect(await entropy.balanceOf(await buyer2.getAddress())).to.eq(0);    
-    await expect(entropy.ownerOf(3)).to.be.revertedWith("ERC721: owner query for nonexistent token");
-    await expect(entropy.connect(buyer2).purchaseCard(3, 5, {value: startPrice})).not.to.be.reverted;
-    await expect(await entropy.balanceOf(await buyer2.getAddress())).to.eq(1);    
-    expect(await entropy.ownerOf(3)).to.be.eq(await buyer2.getAddress());
-    expect(await entropy.tokenURI(3)).to.be.eq('ipfs://foo/3.json');
-
-    const block = await ethers.provider.getBlockNumber();
-    const events = await entropy.queryFilter(
-      entropy.filters.CardPurchased(
-        null,
-        null,
-        null,
-        null,        
-      ),
-      block
+    await expect(await entropy.balanceOf(await buyer2.getAddress())).to.eq(0);
+    await expect(entropy.ownerOf(3)).to.be.revertedWith(
+      "ERC721: owner query for nonexistent token"
     );
-    expect(events.length).eq(1);
-    const eventDetails = entropy.interface.parseLog(events[0]);
-    expect(eventDetails.name).to.eq("CardPurchased");            
-    expect(eventDetails.args.deck).to.eq(3);
-    expect(eventDetails.args.generation).to.eq(5);
-    expect(eventDetails.args.tokenId).to.eq(3);
-    expect(eventDetails.args.purchaser).to.eq(await buyer2.getAddress());
+    await expect(
+      entropy.connect(buyer2).purchaseCard(3, 5, { value: startPrice })
+    )
+      .to.emit(entropy, "CardPurchased")
+      .withArgs(3, 5, 3, await buyer2.getAddress());
+    await expect(await entropy.balanceOf(await buyer2.getAddress())).to.eq(1);
+    expect(await entropy.ownerOf(3)).to.be.eq(await buyer2.getAddress());
+    expect(await entropy.tokenURI(3)).to.be.eq("ipfs://foo/3.json");
   });
 
-
-
-  // it("Allows auctioneer to start auctions for entire generation.", async function () {
-  //   const entropy = await deploy(owner);
-  //   await expect(entropy.ownerOf(0)).to.be.revertedWith("ERC721: owner query for nonexistent token");
-  //   let auction = await entropy._auctions(0);
-  //   await expect(auction.deck).to.be.eq(0);
-  //   const startTime = getNow() - ONE_HOUR;
-  //   await expect(entropy.createAuctionForGeneration(0, startTime))
-  //     .to.be.revertedWith('InvalidGeneration()');
-  //   await expect(entropy.createAuctionForGeneration(1, startTime))
-  //     .not.to.be.reverted;      
-  //   auction = await entropy._auctions(0);
-  //   await expect(auction.deck).to.be.eq(1);
-  
-  // });
-
-  // it("Allows user to make purchase on running auction", async function () {
-  //   const entropy = await deploy(buyer1);
-  //   const startTime = getNow() - ONE_HOUR;
-    
-  //   await expect(
-  //     entropy.connect(owner).createAuctionForGeneration(1, startTime)
-  //   ).not.to.be.reverted;      
-  //   await expect(entropy.ownerOf(0)).to.be.revertedWith("ERC721: owner query for nonexistent token");
-  //   await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(0);
-  //   await expect(entropy.purchaseCard(0)).to.be.revertedWith('InsufficientFunds()');
-  //   await expect(entropy.purchaseCard(0, {value: startPrice})).not.to.be.reverted;
-  //   await expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(1);
-
-  //   const block = await ethers.provider.getBlockNumber();
-  //   const events = await entropy.queryFilter(
-  //     entropy.filters.CardPurchased(
-  //       null,
-  //       null,
-  //       null,
-  //       null,        
-  //     ),
-  //     block
-  //   );
-  //   expect(events.length).eq(1);
-  //   const eventDetails = entropy.interface.parseLog(events[0]);
-  //   expect(eventDetails.name).to.eq("CardPurchased");
-  //   expect(eventDetails.args.auctionId).to.eq(0);
-  //   expect(eventDetails.args.purchaser).to.eq(await buyer1.getAddress());
-  //   expect(eventDetails.args.deck).to.eq(1);
-  //   expect(eventDetails.args.generation).to.eq(1);
-  // });
-
-  // it("Allows auctioneer to settle purchase", async function () {        
-  //   // await entropy.connect(owner).settleAuction(0, startPrice, 'foo');
-  //   expect(await entropy.balanceOf(await buyer1.getAddress())).to.eq(1);
-  //   expect(await entropy.ownerOf(1)).to.be.eq(await buyer1.getAddress())
-  //   expect(await entropy.tokenURI(1)).to.be.eq('ipfs://foo/1.json');
-
-  //   const block = await ethers.provider.getBlockNumber();
-
-  //   const creationEvents = await entropy.queryFilter(
-  //     entropy.filters.AuctionCreated(
-  //       null,
-  //       null,
-  //       null,
-  //       null,        
-  //     ),
-  //     block
-  //   );
-
-  //   expect(creationEvents.length).eq(1);
-  //   let eventDetails = entropy.interface.parseLog(creationEvents[0]);
-  //   expect(eventDetails.name).to.eq("AuctionCreated");
-  //   expect(eventDetails.args.auctionId).to.eq(50);
-  //   expect(eventDetails.args.creator).to.eq(await buyer1.getAddress());
-  //   expect(eventDetails.args.deck).to.eq(1);
-  //   expect(eventDetails.args.generation).to.eq(2); 
-  // });
-
-  // it("Allows chain purchases", async function () {        
-  //   // Make sure random buyer can't execute a chain purchase.    
-  //   await expect(
-  //     entropy.connect(buyer2).purchaseCard(50, {value: startPrice})
-  //   ).to.be.revertedWith('AuctionNotStarted()');
-
-  //   await expect((await entropy._auctions(50)).tokenId).to.eq(0);    
-  //   await expect(
-  //     entropy.connect(buyer1).purchaseCard(50, {value: startPrice})
-  //   ).not.to.be.reverted;
-  //   await expect((await entropy._auctions(50)).startTime).to.eq(0);
-  //   await expect(await (await entropy._auctions(51)).prevPurchaser).to.eq(await buyer1.getAddress());
-  // });
-
-  // it("Allows auctioneer to set rarity", async () => {
-  //   const rarity = [];
-  //   for (let i = 0; i < 3000; i++) {
-  //     rarity[i] = 5;
-  //   }
-  //   await expect(entropy.connect(owner).setRarity(rarity)).not.to.be.reverted;
-  //   expect(await entropy._rarity(2999)).to.be.eq(5);
-  // });
-
+  it("Get the current purchase price", async () => {
+    const startTime = getNow();
+    await ethers.provider.send("evm_setNextBlockTimestamp", [startTime]);
+    await ethers.provider.send("evm_mine", []);
+    await expect(await entropy._price(1, 1, startTime)).to.be.eq(
+      ethers.utils.parseEther("1.5")
+    );
+  });
 });
